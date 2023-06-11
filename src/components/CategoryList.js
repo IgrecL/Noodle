a7719aeba2ea9157d025c35bd8046bcb71e2d832
alaalouj <adamlaalouj.pro@gmail.com>
2023-06-11 14:28:28 +0200

    Repush fenetre categories plus fermeture fenetre

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -2,23 +2,39 @@ import React, { useState } from 'react';
 import Carousel from './Carousel';
 import HoverTitle from './HoverTitle';
 import '../styles/CategoryList.css';
+import UEPagePopping from './UEPagePopping';
 
+const CategoryList = ({ courses }) => {
+  const [selectedUE, setSelectedUE] = useState(null);
 
-const CategoryList = ({courses}) => {
-	return (
-		<div>
-			<ul className="category-list">
-				{courses.map((category, index) => (
-					<li key={index} className="list-element">
-						<div className="category-title">
-							<HoverTitle text1={category[0].UE} text2="Tout explorer" />
-						</div>
-						<Carousel courses={category} />
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+  const handleClick = (UE) => {
+    console.log('handleClick called with UE:', UE);
+    setSelectedUE(UE);
+    document.body.style.overflow = 'hidden';
+  };
+
+  return (
+    <div>
+      <ul className='category-list'>
+        {courses.map((category, index) => (
+		      <div key={index} className="category-item">
+            {selectedUE === category[0].UE && (
+              <UEPagePopping UE={category[0].UE} />
+            )}
+            <li key={index} className='list-element'>
+              <div className='category-title' onClick={() => handleClick(category[0].UE)}>
+                <HoverTitle
+                  text1={category[0].UE}
+                  text2='Tout explorer'
+                />
+              </div>
+              <Carousel courses={category} />
+			      </li>
+          </div>		  
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CategoryList;
--- src/components/PagePopping.js
+++ src/components/PagePopping.js
@@ -1,11 +1,23 @@
-import '../styles/PagePopping.css'
+import '../styles/PagePopping.css';
+import React, { useEffect, useState } from 'react';
 
 const PagePopping = ({ course, index }) => {
+
+    const [isWindowOpen, setIsWindowOpen] = useState(true);
+
+    const handleCloseClick = () => {
+        setIsWindowOpen(false);
+    };
+
+    if (!isWindowOpen) {
+      return null;
+    }
+
     return (
         <div className='page-container'>
             <div className='page-pop'> 
             <img src={course.image} alt={`img ${index}`} className='image-page'></img>
-            <button className='close'>✖</button>
+            <button className='close' onClick={handleCloseClick}>✖</button>
             </div>
         </div>
     );
