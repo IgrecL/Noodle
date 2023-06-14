b21efc14c73d47128efab65ecdd27b28bdc0a255
alaalouj <adamlaalouj.pro@gmail.com>
2023-06-14 08:10:52 +0200

    Début titre et description pages cours

--- src/components/PagePopping.js
+++ src/components/PagePopping.js
@@ -20,6 +20,16 @@ const PagePopping = ({ course, index, reset }) => {
         <div className='page-container'>
             <div className='page-pop'> 
             <img src={course.image} alt={`img ${index}`} className='image-page'></img>
+            <h1>{course.title}</h1>
+			<div className='buttons'>
+				<a href='https://docs.google.com/presentation/d/1YUjNjfnHAnkmmquXGP7QWRpZiAj8vzNtkU2Ud4TGg-w/edit#slide=id.p' target="_blank" rel="noreferrer">
+					<button className='button1'>▶ Lecture</button>
+				</a>
+				<a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer">
+					<button className='button2'>+</button>
+				</a>
+			</div>
+            <p>{course.description}</p>
             <button className='close' onClick={handleCloseClick}>✖</button>
             </div>
         </div>
