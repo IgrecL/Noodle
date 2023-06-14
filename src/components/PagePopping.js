fd3082237d77e0416a3e6f1a47fddab81158ba28
Valentin Renaud <valentinrenaud@MacBook-Air-de-Valentin.local>
2023-06-14 16:35:38 +0200

    Amélioration Front PagePopping v2

--- src/components/PagePopping.js
+++ src/components/PagePopping.js
@@ -22,16 +22,17 @@ const PagePopping = ({ course, index, reset }) => {
     return (
         <div className='page-container'>
             <div className='page-pop'> 
-            <div style={imageRef} className='image-page'></div>
+            <div style={imageRef} className='image-page'>
+                <div className='buttons-pop'>
+				    <a href='https://docs.google.com/presentation/d/1YUjNjfnHAnkmmquXGP7QWRpZiAj8vzNtkU2Ud4TGg-w/edit#slide=id.p' target="_blank" rel="noreferrer">
+					    <button className='button1'>▶ Lecture</button>
+				    </a>
+				    <a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer">
+					    <button className='button2'>+</button>
+				    </a>
+			    </div>
+            </div>
             <h1 className='title-pop'>{course.title}</h1>
-			<div className='buttons-pop'>
-				<a href='https://docs.google.com/presentation/d/1YUjNjfnHAnkmmquXGP7QWRpZiAj8vzNtkU2Ud4TGg-w/edit#slide=id.p' target="_blank" rel="noreferrer">
-					<button className='button1'>▶ Lecture</button>
-				</a>
-				<a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer">
-					<button className='button2'>+</button>
-				</a>
-			</div>
             <p className='description-pop'>{course.description}</p>
             <button className='close' onClick={handleCloseClick}>✖</button>
             </div>
