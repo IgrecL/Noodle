72494760edb47828eb5950aded920c7edb63441a
haddent <adam.aboucaya@gmail.com>
2023-06-14 10:02:10 +0200

    Scroll adapt

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,10 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import '../styles/Carousel.css';
 import '../styles/title-buttons.css';
 import PagePopping from './PagePopping';
 
 const Carousel = ({ courses }) => {
   const carouselRef = useRef(null);
+  const [selectedCourseIndex, setSelectedCourseIndex] = useState(null);
+  const [isOverflow, setIsOverflow] = useState(false);
 
   const SCROLL_AMOUNT = 1035; // Adjust the scroll amount as needed
 
@@ -22,44 +24,65 @@ const Carousel = ({ courses }) => {
     });
   };
 
-  const [selectedCourseIndex, setSelectedCourseIndex] = useState(null);
-
   const handleClick = (index) => {
     setSelectedCourseIndex(index);
     document.body.style.overflow = 'hidden';
   };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const carouselContainer = carouselRef.current;
+      setIsOverflow(carouselContainer.scrollWidth > carouselContainer.offsetWidth);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    const carouselContainer = carouselRef.current;
+    const handleOverflow = () => {
+      const isOverflowing = carouselContainer.scrollWidth > carouselContainer.offsetWidth;
+      setIsOverflow(isOverflowing);
+      if (!isOverflowing) {
+        carouselContainer.scrollLeft = 0;
+      }
+    };
+
+    handleOverflow();
+    window.addEventListener('resize', handleOverflow);
+    return () => {
+      window.removeEventListener('resize', handleOverflow);
+    };
+  }, []);
 
   return (
     <div className="image-carousel">
       <button className="carousel-button left" onClick={scrollLeft}>
         &lt;
       </button>
-      <div className="carousel-container" ref={carouselRef}>
+      <div
+        className={`carousel-container${isOverflow ? ' overflow-scroll' : ''}`}
+        ref={carouselRef}
+      >
         {courses.map((course, index) => (
           <div key={index} className="carousel-item">
             {selectedCourseIndex === index && (
-              <PagePopping course={course} index={index} reset = {setSelectedCourseIndex} />
+              <PagePopping course={course} index={index} reset={setSelectedCourseIndex} />
             )}
-            <button
-              key={index}
-              className="image-wrapper"
-            >
-              <img
-                src={course.image}
-                alt={`Image ${index}`}
-                className="carousel-image"
-              />
+            <button key={index} className="image-wrapper">
+              <img src={course.image} alt={`Image ${index}`} className="carousel-image" />
               <div className="carousel-title">
-              {course.shortTitle}
-              <button className='list-button'>+</button>
-              <div className='list-rectangle'>Ajouter à ma liste</div>
-              <button 
-                className='arrow-button'
-                onClick={() => handleClick(index)}
-              >
-                ˅
-              </button>
-              <div className='list-know'>En savoir plus</div>
+                {course.shortTitle}
+                <button className="list-button">+</button>
+                <div className="list-rectangle">Ajouter à ma liste</div>
+                <button className="arrow-button" onClick={() => handleClick(index)}>
+                  ˅
+                </button>
+                <div className="list-know">En savoir plus</div>
               </div>
             </button>
           </div>
