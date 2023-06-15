9d88d49236693c1da3b59b1005518bf8a3622818
IgrecL <ngowg5p3v@relay.firefox.com>
2023-06-15 09:23:35 +0200

    Better loading but it works

--- src/components/PagePopping.js
+++ src/components/PagePopping.js
@@ -37,27 +37,32 @@ const PagePopping = ({ course, reset }) => {
 
     return (
         <div className='page-container'>
-            {linksList[0] ? (
-                <div className='page-pop'> 
-                <div style={imageRef} className='image-page'>
-                    <div className='fade-image-page'/>
-                    <h1 className='title-pop'>{course.title}</h1>
-                        <div className='buttons-pop'>
-                            <a href={linksList[0].link} target="_blank" rel="noreferrer">
-                                <button className='button1'>▶ Lecture</button>
-                            </a>
-                            <a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer">
-                                <button className='button2'>+</button>
-                            </a>
-                        </div>
-                </div>
-                <p className='description-pop'>{course.description}</p>
-                <div className='linksList-pop'>
-                    <LinksList links={linksList} />
-                </div>
-                <button className='close' onClick={handleCloseClick}>✖</button>
-                </div>
-            ) : ("")}
+            <div className='page-pop'> 
+            <div style={imageRef} className='image-page'>
+                <div className='fade-image-page'/>
+                <h1 className='title-pop'>{course.title}</h1>
+                {linksList[0] ? (
+                    <div className='buttons-pop'>
+                        <a href={linksList[0].link} target="_blank" rel="noreferrer">
+                            <button className='button1'>▶ Lecture</button>
+                        </a>
+                        <a href='https://en.wikipedia.org/wiki/React_(software)' target="_blank" rel="noreferrer">
+                            <button className='button2'>+</button>
+                        </a>
+                    </div>
+                ) : (
+                    <div className='buttons-pop'>
+                        <button className='button1'>▶ Lecture</button>
+                        <button className='button2'>+</button>
+                    </div>
+                )}
+            </div>
+            <p className='description-pop'>{course.description}</p>
+            <div className='linksList-pop'>
+                <LinksList links={linksList} />
+            </div>
+            <button className='close' onClick={handleCloseClick}>✖</button>
+            </div>
         </div>
     );
 }
